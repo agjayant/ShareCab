5fe992e19168c652dc8a11748afb4e7f0ff6ac78
Rishabh Goyal <risgoyal@iitk.ac.in>
2016-11-01 17:32:56 +0530

    set up basic database and display for admin

--- sharecabapp/models.py
+++ sharecabapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Ride(models.Model):
+    destination = models.CharField(max_length=30)
+    source =  models.CharField(max_length=30)
+    ridetime = models.DateTimeField('leave time')
+    capacity = models.IntegerField(default=0)
+    def __unicode__(self):
+        return self.destination
+
+class Person(models.Model):
+    ridegroup = models.ForeignKey(Ride)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    def __unicode__(self):
+        return self.name
