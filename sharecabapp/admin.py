3fed96480526b4ca809d01d307ae6dfe878063f4
Rishabh Goyal <risgoyal@iitk.ac.in>
2016-11-04 11:09:18 +0530

    added filtering with time, search box for source and destination, changed header

--- sharecabapp/admin.py
+++ sharecabapp/admin.py
@@ -11,6 +11,8 @@ class PersonInline(admin.TabularInline):
 class RideAdmin(admin.ModelAdmin):
     inlines = [PersonInline]
     list_display = ('destination', 'source', 'ridetime', 'capacity')
+    list_filter = ['ridetime']
+    search_fields = ['destination','source']
 
 admin.site.register(Ride, RideAdmin)
 #admin.site.register(Person)
